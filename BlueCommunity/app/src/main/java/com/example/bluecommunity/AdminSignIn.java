34bcb15e08509f22e863043b9942bb448d1a2680
Toves99 <114761179+Toves99@users.noreply.github.com>
2023-03-22 14:24:31 +0300

    admin signin page update

--- BlueCommunity/app/src/main/java/com/example/bluecommunity/AdminSignIn.java
+++ BlueCommunity/app/src/main/java/com/example/bluecommunity/AdminSignIn.java
@@ -13,25 +13,53 @@ import android.widget.Toast;
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.database.DataSnapshot;
 import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
 import com.google.firebase.database.ValueEventListener;
+import com.google.firebase.firestore.FirebaseFirestore;
+
+import java.util.regex.Pattern;
 
 public class AdminSignIn extends AppCompatActivity {
-    DatabaseReference databaseReference= FirebaseDatabase.getInstance().getReferenceFromUrl("https://sunrisecommunity-95d96-default-rtdb.firebaseio.com/");
+    FirebaseAuth firebaseAuth;
+    FirebaseFirestore firebaseFirestore;
+    private static final Pattern PASSWORD_PATTERN =
+            Pattern.compile("^" +
+                    "(?=.*[@#$%^&+=])" +
+                    "(?=\\S+$)" +
+                    ".{6,}" +
+                    "$"
+            );
+    /**
+     developer clinton tovesi
+     *
+     */
+    //variables declaration
     private EditText etUserName;
     private EditText etEmail;
     private EditText etPassword;
     private Button signBtn;
     ProgressDialog progressDialog;
+    public void onBackPressed(){
+        super.onBackPressed();
+        startActivity(new Intent(AdminSignIn.this,AdminLogin.class));
+        finish();
+    }
     @SuppressLint("MissingInflatedId")
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_admin_sign_in);
         progressDialog=new ProgressDialog(this);
+        //initializing id
+        firebaseAuth=FirebaseAuth.getInstance();
+        firebaseFirestore=FirebaseFirestore.getInstance();
         etUserName=findViewById(R.id.etUserName);
         etEmail=findViewById(R.id.etEmail);
         etPassword=findViewById(R.id.etPassword);
@@ -43,10 +71,11 @@ public class AdminSignIn extends AppCompatActivity {
             }
         });
     }
+    //validating inputs from users
     public boolean validateUserName(){
-        String userName=etUserName.getText().toString();
-        if(userName.isEmpty()){
-            etUserName.setError("Enter your Username");
+        String username=etUserName.getText().toString();
+        if(username.isEmpty()){
+            etUserName.setError("Provide userName please");
             etUserName.requestFocus();
             return false;
         }
@@ -55,22 +84,10 @@ public class AdminSignIn extends AppCompatActivity {
             return true;
         }
     }
-    public boolean validatePassword(){
-        String password=etPassword.getText().toString();
-        if(password.isEmpty()){
-            etPassword.setError("Enter your Password");
-            etPassword.requestFocus();
-            return false;
-        }
-        else {
-            etPassword.setError(null);
-            return true;
-        }
-    }
     public boolean validateEmail(){
         String email=etEmail.getText().toString();
         if(email.isEmpty()){
-            etEmail.setError("Enter your Email");
+            etEmail.setError("Provide your email please");
             etEmail.requestFocus();
             return false;
         }
@@ -84,44 +101,59 @@ public class AdminSignIn extends AppCompatActivity {
             return true;
         }
     }
+    public boolean validatePassword(){
+        String password=etPassword.getText().toString();
+        if(password.isEmpty()){
+            etPassword.setError("Provide your password please");
+            etPassword.requestFocus();
+            return false;
+        }
+        else if(!PASSWORD_PATTERN.matcher(password).matches()){
+            etPassword.setError("Please enter a strong password");
+            etPassword.requestFocus();
+            return false;
+        }
+        else if(password.length()<8){
+            etPassword.setError("Password is too short");
+            etPassword.requestFocus();
+            return false;
+        }
+        else {
+            etPassword.setError(null);
+            return true;
+        }
+
+    }
+    //Method for registering
     public void sign(){
-        String userName=etUserName.getText().toString();
+        String username=etUserName.getText().toString();
         String email=etEmail.getText().toString();
         String password=etPassword.getText().toString();
-        if(!validateUserName()|!validatePassword()|!validateEmail()){
+        if(!validateUserName()|!validateEmail()|!validatePassword()){
             return;
         }
         else {
-            databaseReference.addListenerForSingleValueEvent(new ValueEventListener() {
-                @Override
-                public void onDataChange(@NonNull DataSnapshot snapshot) {
-                    if(snapshot.hasChild(email)){
-                        progressDialog.setMessage("Creating Your account");
-                        progressDialog.setTitle("Please Wait...");
-                        progressDialog.setIndeterminate(true);
-                        progressDialog.show();
-                        etEmail.setError("Email already exist");
-                        progressDialog.dismiss();
-                    }
-                    else {
-                        databaseReference.child("admin").child(email).child("email").setValue(userName);
-                        databaseReference.child("admin").child(email).child("password").setValue(password);
-                        progressDialog.setMessage("Creating Your Account");
-                        progressDialog.setTitle("Please Wait...");
-                        progressDialog.setIndeterminate(true);
-                        progressDialog.show();
-                        Toast.makeText(AdminSignIn.this,"Account created",Toast.LENGTH_SHORT).show();
-                        Intent intent=new Intent(AdminSignIn.this,AdminLogin.class);
-                        startActivity(intent);
-                        finish();
-                    }
-                }
-
-                @Override
-                public void onCancelled(@NonNull DatabaseError error) {
-                    Toast.makeText(AdminSignIn.this,"Check your connection",Toast.LENGTH_SHORT).show();
-                }
-            });
+            progressDialog.show();
+            firebaseAuth.createUserWithEmailAndPassword(email,password)
+                    .addOnSuccessListener(new OnSuccessListener<AuthResult>() {
+                        @Override
+                        public void onSuccess(AuthResult authResult) {
+                            startActivity(new Intent(AdminSignIn.this,AdminLogin.class));
+                            finish();
+                            progressDialog.cancel();
+                            firebaseFirestore.collection("user")
+                                    .document(FirebaseAuth.getInstance().getUid())
+                                    .set(new UserModel(username,email,password));
+                        }
+                    })
+                    .addOnFailureListener(new OnFailureListener() {
+                        @Override
+                        public void onFailure(@NonNull Exception e) {
+                            Toast.makeText(AdminSignIn.this,e.getMessage(),Toast.LENGTH_SHORT).show();
+                            progressDialog.cancel();
+                        }
+                    });
         }
+
     }
 }
